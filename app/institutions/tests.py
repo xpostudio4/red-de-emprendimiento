a5ede7bf978bf698756fe0219ee134f995511882
unknown <wilson.campusano@MININT-CQTMJ1A.ARLSS.DO>
2014-09-01 09:18:23 -0400

    user administration approval triangulation

--- app/institutions/tests.py
+++ app/institutions/tests.py
@@ -1,9 +1,22 @@
 from django.test import TestCase
 from institutions import User
 
-class UserTestCase(TestCase):
-	def test_user_needs_administrator_approval(self):
-		user = User()
+class UserAdministrationTestCase(TestCase):
+
+	def setUp(self):
 		user_administration_context = UserAdministrationContext()
+	
+	def test_user_add_administrator_approval(self):
+	
+		user = User()
+		
+		self.assertEqual(user.approved, False)
 		user_administration_context.approve(user)		
 		self.assertEqual(u.approved, True)
+
+	def test_user_remove_administration_approval(self):
+		user = User()
+
+		user_administration_context.approved(user)
+		self.assertEqual(user.approved, True)
+		user_administration_context.unapprove(user)
