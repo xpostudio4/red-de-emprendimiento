cf167e6845af0add13a5ab5f983a2c00c1821d8c
unknown <wilson.campusano@MININT-CQTMJ1A.ARLSS.DO>
2014-09-01 09:08:59 -0400

    test case for user approval

--- app/institutions/tests.py
+++ app/institutions/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from institutions import User
 
-# Create your tests here.
+class UserTestCase(TestCase):
+	def test_user_needs_administrator_approval(self):
+		user = User()
+		user_administration_context = UserAdministrationContext()
+		user_administration_context.approve(user)		
+		self.assertEqual(u.approved, True)
