981a2fab8fb614640fee57339c7033aeb77dfbd1
unknown <wilson.campusano@MININT-CQTMJ1A.ARLSS.DO>
2014-09-01 10:40:24 -0400

    more readable test methods

--- app/institutions/tests.py
+++ app/institutions/tests.py
@@ -5,7 +5,7 @@ class UserAdministrationTestCase(TestCase):
 	def setUp(self):
 		self.user_administration_context = UserAdministrationContext()
 	
-	def test_user_add_administrator_approval(self):
+	def test_administrator_add_approval_to_user(self):
 
 		user = User()
 		
@@ -13,7 +13,7 @@ class UserAdministrationTestCase(TestCase):
 		self.user_administration_context.approve(user)		
 		self.assertEqual(u.approved, True)
 
-	def test_user_remove_administration_approval(self):
+	def test_administrator_remove_approval_to_user(self):
 		user = User()
 
 		self.user_administration_context.approved(user)
