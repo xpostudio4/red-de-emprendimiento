5ec07e50bfdbf61689c9693344d696ad3b6320aa
unknown <wilson.campusano@MININT-CQTMJ1A.ARLSS.DO>
2014-09-01 09:33:18 -0400

    adding travis config for continuos integration

--- app/institutions/tests.py
+++ app/institutions/tests.py
@@ -4,19 +4,19 @@ from institutions import User
 class UserAdministrationTestCase(TestCase):
 
 	def setUp(self):
-		user_administration_context = UserAdministrationContext()
+		self.user_administration_context = UserAdministrationContext()
 	
 	def test_user_add_administrator_approval(self):
-	
+
 		user = User()
 		
 		self.assertEqual(user.approved, False)
-		user_administration_context.approve(user)		
+		self.user_administration_context.approve(user)		
 		self.assertEqual(u.approved, True)
 
 	def test_user_remove_administration_approval(self):
 		user = User()
 
-		user_administration_context.approved(user)
+		self.user_administration_context.approved(user)
 		self.assertEqual(user.approved, True)
 		user_administration_context.unapprove(user)
