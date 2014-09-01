5e07878ac8374ab1628a527e9cd0badd3c28af10
unknown <wilson.campusano@MININT-CQTMJ1A.ARLSS.DO>
2014-09-01 10:07:38 -0400

    using UserProfile class from models

--- app/institutions/tests.py
+++ app/institutions/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from institutions import User
+from institutions.models import UserProfile
 
 class UserAdministrationTestCase(TestCase):
 
@@ -8,14 +8,14 @@ class UserAdministrationTestCase(TestCase):
 	
 	def test_user_add_administrator_approval(self):
 
-		user = User()
+		user = UserProfile()
 		
 		self.assertEqual(user.approved, False)
 		self.user_administration_context.approve(user)		
 		self.assertEqual(u.approved, True)
 
 	def test_user_remove_administration_approval(self):
-		user = User()
+		user = UserProfile()
 
 		self.user_administration_context.approved(user)
 		self.assertEqual(user.approved, True)
