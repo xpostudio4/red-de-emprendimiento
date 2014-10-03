5f81c36164cdfdfe32c5fe604735939169f3b05a
Vivian Guillen <vivianguillen@Vivians-MacBook-Air.local>
2014-10-03 11:38:09 -0400

    added companies

--- app/app/urls.py
+++ app/app/urls.py
@@ -8,10 +8,10 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', TemplateView.as_view(template_name='base.html')),
+    url(r'^$', 'app.views.home'),
 
     # Examples:
-    url(r'^$', 'app.views.home', name='home'),
+    url(r'^inspira/', 'app.views.inspire'),
     # url(r'^app/', include('app.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
--- app/app/views.py
+++ app/app/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'site/index.html')
+
+def inspire(request):
+    return render(request, 'site/inspira.html')
