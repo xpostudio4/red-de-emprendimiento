625414585c13ac7c751c9bd7c9afba720e249221
Leonardo Jimenez <ljimenez@stancedata.com>
2015-02-18 15:31:11 -0400

    add middleware file

--- app/app/settings/base.py
+++ app/app/settings/base.py
@@ -169,6 +169,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'institutions.middleware.LoginFormMiddleware',
 )
 ########## END MIDDLEWARE CONFIGURATION
 
---
+++ app/institutions/middleware.py
@@ -0,0 +1,16 @@
+from .form import UserProfileLoginForm
+
+
+class LoginFormMiddleware(object):
+    """
+    The purpose of this middleware is to include the login form in every GET
+    request
+    """
+    def process_request(self, request):
+        """Process the request if the method is Method is get, provide a login
+        form"""
+        if request.method == 'GET':
+            login_form = UserProfileLoginForm()
+            request.login_form = login_form
+        else:
+            pass
