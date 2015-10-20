e0c4b2d7323b708343ce20234c90e682ffe976f4
Leonardo <leonardo@codetiger.co>
2015-10-20 16:38:27 -0400

    fixes #31

--- app/institutions/admin.py
+++ app/institutions/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Organization, UserProfile, MailingList
 
-# Register your models here.
+class OrganizationAdmin(admin.ModelAdmin):
+    pass
+
+class UserProfileAdmin(admin.ModelAdmin):
+    pass
+
+class MailingListAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Organization, OrganizationAdmin)
+admin.site.register(UserProfile, UserProfileAdmin)
+admin.site.register(MailingList, MailingListAdmin)
